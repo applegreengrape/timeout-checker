e4c564c2d3197f8c514c0d275257b3dc7ee5c327
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-19 12:53:17 +0000

    Brew formula update for delay-api version v0.0.3

--- Formula/delay-api.rb
+++ Formula/delay-api.rb
@@ -5,13 +5,13 @@
 class DelayApi < Formula
   desc "simple wait& response api for timeout test"
   homepage "https://github.com/applegreengrape/delay-api"
-  version "0.0.2"
+  version "0.0.3"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/applegreengrape/delay-api/releases/download/v0.0.2/delay-api_0.0.2_darwin_amd64.zip"
-      sha256 "afd4c63c09a64f633de447c2eccabe99c1af9af140e26e0a3ed77851079b22dc"
+      url "https://github.com/applegreengrape/delay-api/releases/download/v0.0.3/delay-api_0.0.3_darwin_amd64.zip"
+      sha256 "6509133acbc16535c6d64fb9a249bf3f96939c8b63d64bf553fd9814880a1274"
 
       def install
         bin.install "wait"
