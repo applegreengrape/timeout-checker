bdd3efa2a9b623bc40c594830cd1dc8627478d52
pingzhou <pingzhou.liu@slalom.com>
2021-11-25 16:30:48 +0000

    add more flags

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"flag"
-	"time"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -14,9 +16,13 @@ type Wait struct {
     time int
 }
 
+type Echo struct {
+	time int
+	payloadPath string
+}
+
 func (wait *Wait) healthcheck(w http.ResponseWriter, r *http.Request) {
 	log.Printf("wait %d second", wait.time)
-	log.Println("received")
 	time.Sleep(time.Duration(wait.time) * time.Second)
 
 	w.WriteHeader(http.StatusOK)
@@ -24,15 +30,37 @@ func (wait *Wait) healthcheck(w http.ResponseWriter, r *http.Request) {
 	log.Println("returned")
 }
 
+func (echo *Echo) echo(w http.ResponseWriter, r *http.Request) {
+	log.Printf("wait %d second", echo.time)
+	log.Println("received")
+	time.Sleep(time.Duration(echo.time) * time.Second)
+
+	w.WriteHeader(http.StatusOK)
+	jsonFile, err := os.Open(echo.payloadPath)
+    if err != nil {}
+
+	defer jsonFile.Close()
+
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+	w.Write([]byte(byteValue))
+	log.Println("returned")
+}
+
 func main() {
 	wait := flag.Int("wait", 3, "")
 	port := flag.Int("port", 8080, "")
+	path := flag.String("path", "/api/echo", "")
+	method := flag.String("method", "POST", "")
+	payload := flag.String("payload", "", "path to payload json file")
 	
 	flag.Parse()
 
 	w := &Wait{time: *wait}
+	e := &Echo{time: *wait, payloadPath: *payload}
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/healthcheck", w.healthcheck).Methods("GET")
+	router.HandleFunc(*path, e.echo).Methods(*method)
+
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), router)
 }
